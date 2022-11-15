85cf5aa74872b2900e531530379adbe89720cc2b
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-15 04:38:30 -0700

    commit tree d2df49282b321b4cbca72b89d016cf99c2e2a0f2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+  def kind
+
+    is_a_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a  == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  private
+
+  def is_a_triangle
+    raise TriangleError unless positive_triangle_length? && triangle_inequality?
+  end
+
+  def positive_triangle_length?
+    a > 0 && b > 0 && c > 0 
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+
+  class TriangleError < StandardError
+
+  end
+
+
+
 end
